27989a68ffc3b25d7dc6aa2a00233cdbd50be15f
ocavue <ocavue@gmail.com>
2025-08-19 23:13:53 +1000

    update build scripts

--- packages/svelte/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-
-import { viteConfigFactory } from '../../vite.config'
-
-export default viteConfigFactory(import.meta.url, {
-  plugins: [svelte()],
-})
