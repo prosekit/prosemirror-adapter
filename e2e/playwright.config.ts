7d09e58a311a5f2cacfe610d1f9938d113eeeec8
ocavue <ocavue@gmail.com>
2025-08-20 03:22:54 +1000

    enable retries on local env

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -17,8 +17,8 @@ export default defineConfig({
   testDir: './tests',
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  /* Retry */
+  retries: process.env.CI ? 2 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
