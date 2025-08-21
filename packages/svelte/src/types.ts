b9fea4b2874a9129eeb2796e63f77c18edd85566
ocavue <ocavue@gmail.com>
2025-08-22 09:21:25 +1000

    fix(svelte): fix svelte component types

--- packages/svelte/src/types.ts
+++ packages/svelte/src/types.ts
@@ -1,11 +1,9 @@
 import type { Component, ComponentConstructorOptions, SvelteComponent } from 'svelte'
 
 export type AnyRecord = Record<string, any>
-export type SvelteClassComponentConstructor<T extends AnyRecord = AnyRecord> = new (
+export type SvelteClassComponentConstructor<T extends AnyRecord = any> = new (
   options: ComponentConstructorOptions<T>,
 ) => SvelteComponent
-export type SvelteComponentConstructor<T extends AnyRecord = AnyRecord> =
-  | SvelteClassComponentConstructor<T>
-  | Component<T>
+export type SvelteComponentConstructor<T extends AnyRecord = any> = SvelteClassComponentConstructor<T> | Component<T>
 
 export type Obj2Map<T extends AnyRecord> = Map<keyof T, T[keyof T]>
