f39149cfa095a172c91319c31e3869be323dac7b
ocavue <ocavue@gmail.com>
2025-09-26 20:52:56 +1000

    Solid

---
+++ e2e2/src/solid/components/Editor.tsx
@@ -0,0 +1,76 @@
+import {
+  useMarkViewFactory,
+  useNodeViewFactory,
+  usePluginViewFactory,
+  useWidgetViewFactory,
+} from '@prosemirror-adapter/solid'
+import { Plugin } from 'prosemirror-state'
+import { DecorationSet } from 'prosemirror-view'
+
+import { createEditorView } from '../../shared/createEditorView'
+
+import { Hashes } from './Hashes'
+import { Heading } from './Heading'
+import { Link } from './Link'
+import { Paragraph } from './Paragraph'
+import { Size } from './Size'
+
+export function Editor() {
+  const nodeViewFactory = useNodeViewFactory()
+  const markViewFactory = useMarkViewFactory()
+  const widgetViewFactory = useWidgetViewFactory()
+  const pluginViewFactory = usePluginViewFactory()
+
+  const editorRef = (element: HTMLDivElement) => {
+    if (element.firstChild) return
+
+    const getHashWidget = widgetViewFactory({
+      as: 'i',
+      component: Hashes,
+    })
+
+    createEditorView(
+      element,
+      {
+        paragraph: nodeViewFactory({
+          component: Paragraph,
+          as: 'div',
+          contentAs: 'p',
+        }),
+        heading: nodeViewFactory({
+          component: Heading,
+        }),
+      },
+      {
+        link: markViewFactory({
+          component: Link,
+        }),
+      },
+      [
+        new Plugin({
+          view: pluginViewFactory({
+            component: Size,
+          }),
+        }),
+        new Plugin({
+          props: {
+            decorations(state) {
+              const { $from } = state.selection
+              const node = $from.node()
+              if (node.type.name !== 'heading') return DecorationSet.empty
+
+              const widget = getHashWidget($from.before() + 1, {
+                side: -1,
+                level: node.attrs.level as number,
+              })
+
+              return DecorationSet.create(state.doc, [widget])
+            },
+          },
+        }),
+      ],
+    )
+  }
+
+  return <div class="editor" ref={editorRef}></div>
+}
---
+++ e2e2/src/solid/components/Size.tsx
@@ -0,0 +1,9 @@
+import { usePluginViewContext } from '@prosemirror-adapter/solid'
+import { createMemo } from 'solid-js'
+
+export function Size() {
+  const context = usePluginViewContext()
+  const size = createMemo(() => context().view.state.doc.nodeSize)
+
+  return <div data-test-id="size-view-plugin">Size for document: {size()}</div>
+}
