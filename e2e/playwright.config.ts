7f37341b95495849c53b883285f2018952e03813
ocavue <ocavue@gmail.com>
2025-09-26 21:18:16 +1000

    review

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -48,7 +48,7 @@ export default defineConfig({
     },
   ],
 
-  // /* Run your local dev server before starting the tests */
+  /* Run your local dev server before starting the tests */
   webServer: {
     command: 'pnpm run preview',
     url: 'http://localhost:7001',
