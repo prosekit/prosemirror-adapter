f43c68b645345c9be9a0775f4512d53e7fdd81d7
ocavue <ocavue@gmail.com>
2025-09-26 20:18:06 +1000

    add frameworks

--- e2e2/astro.config.mjs
+++ e2e2/astro.config.mjs
@@ -3,7 +3,15 @@ import { defineConfig } from 'astro/config'
 
 import react from '@astrojs/react';
 
+import preact from '@astrojs/preact';
+
+import svelte from '@astrojs/svelte';
+
+import solidJs from '@astrojs/solid-js';
+
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), preact(), svelte(), solidJs(), vue()],
 })
