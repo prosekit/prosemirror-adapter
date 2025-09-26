2a16efcd90c9d8a3c7373328bcc47f1015caff96
ocavue <ocavue@gmail.com>
2025-09-26 22:10:13 +1000

    fix build

--- e2e/astro.config.mjs
+++ e2e/astro.config.mjs
@@ -21,9 +21,9 @@ export default defineConfig({
     '/vue': '/stories/vue/app/vue',
   },
   integrations: [
-    react({ include: ['src/react/**/*'] }),
-    preact({ include: ['src/preact/**/*'] }),
-    solidJs({ include: ['src/solid/**/*'] }),
+    preact({ include: ['src/preact/**/*.tsx'] }),
+    react({ include: ['src/react/**/*.tsx'] }),
+    solidJs({ include: ['src/solid/**/*.tsx'] }),
     svelte(),
     vue(),
     astrobook({ directory: 'src' }),
