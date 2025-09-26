1c3fbd6cd8c57f0c9dcf05f5bdc7cf0b4e2b5259
ocavue <ocavue@gmail.com>
2025-09-26 20:49:58 +1000

    svelte

---
+++ e2e2/src/svelte/App.stories.ts
@@ -0,0 +1,9 @@
+import App from './App.svelte'
+
+export default {
+  component: App,
+}
+
+export const Svelte = {
+  args: {},
+}
---
+++ e2e2/src/svelte/components/Paragraph.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { useNodeViewContext } from '@prosemirror-adapter/svelte'
+  let selected = false
+
+  const contentRef = useNodeViewContext('contentRef')
+  const selectedStore = useNodeViewContext('selected')
+  selectedStore.subscribe((value) => {
+    selected = value
+  })
+</script>
+
+<div use:contentRef class:selected></div>
+
+<style>
+  .selected {
+    outline: blue solid 1px;
+  }
+</style>
