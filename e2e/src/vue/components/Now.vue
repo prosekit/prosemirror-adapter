8770649999efe5ee96a310595d13e711b41527be
ocavue <ocavue@gmail.com>
2025-12-25 21:45:18 +1100

    add test case for vue

---
+++ e2e/src/vue/components/Now.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { useExtraContext } from './context'
+
+const now = useExtraContext()
+</script>
+
+<template>
+  <div data-test-id="now-view-plugin">Now: {{ now }}</div>
+</template>
