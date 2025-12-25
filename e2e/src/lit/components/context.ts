2fcc14699622378e80af845b798de4ed4dba1d25
ocavue <ocavue@gmail.com>
2025-12-25 22:52:48 +1100

    fix test

--- e2e/src/lit/components/context.ts
+++ e2e/src/lit/components/context.ts
@@ -1,3 +1,3 @@
 import { createContext } from '@lit/context'
 
-export const extraContext = createContext<string>(Symbol('extra-context'))
+export const extraContext = createContext<string>('lit-extra-context')
