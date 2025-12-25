cb9f8ae41753578338810f6422b1681b86cbf68a
ocavue <ocavue@gmail.com>
2025-12-25 21:53:57 +1100

    add svelte test

---
+++ e2e/src/svelte/components/Now.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { useExtraContext } from './context.svelte.ts'
+  let now = $state('')
+  const getNow = useExtraContext()
+
+  $effect(() => {
+    now = getNow()
+  })
+</script>
+
+<div data-test-id="now-view-plugin">Now: {now}</div>
