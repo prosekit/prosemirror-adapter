44f78505c2822d01233c2799216b7fa36e30f2c7
ocavue <ocavue@gmail.com>
2025-12-25 22:01:22 +1100

    update test

--- e2e/tests/plugin-view.spec.ts
+++ e2e/tests/plugin-view.spec.ts
@@ -16,7 +16,18 @@ testAll(() => {
 
   test('Render context from parent component', async ({ page }) => {
     const locator = page.locator('.editor [data-test-id="now-view-plugin"]')
+
+    // Expect the <Now> component itself to render
     await expect(locator).toContainText(/Now/)
+
+    // Expect the <Now> component can receive the context from the parent component
     await expect(locator).toContainText(/\d{2}:\d{2}:\d{2}/)
+
+    // Expect the <Now> component can update the context from the parent component
+    const text = await locator.textContent()
+    const now = text?.match(/\d{2}:\d{2}:\d{2}/)?.[0] || ''
+    expect(now).toHaveLength(8)
+    await page.waitForTimeout(1000)
+    await expect(locator).not.toContainText(new RegExp(now))
   })
 })
