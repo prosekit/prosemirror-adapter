fe258207dc6c2dd951713a49fe1d2b1f07ff34b2
ocavue <ocavue@gmail.com>
2026-03-13 01:46:40 +1100

    feat(svelte): drop Svelte v4 support

--- packages/svelte/src/types.ts
+++ packages/svelte/src/types.ts
@@ -1,10 +1,7 @@
-import type { Component, ComponentConstructorOptions, SvelteComponent } from 'svelte'
+import type { Component } from 'svelte'
 
 export type AnyRecord = Record<string, any>
-export type SvelteClassComponentConstructor<T extends AnyRecord = any> = new (
-  options: ComponentConstructorOptions<T>,
-) => SvelteComponent
-export type SvelteComponentConstructor<T extends AnyRecord = any> = SvelteClassComponentConstructor<T> | Component<T>
+export type SvelteComponentConstructor<T extends AnyRecord = any> = Component<T>
 
 /**
  * @internal
