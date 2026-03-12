49a2147f16319615accfb65a738e651521731dc6
ocavue <ocavue@gmail.com>
2026-03-13 04:02:07 +1100

    vue 4

--- packages/vue/src/VueRenderer.ts
+++ packages/vue/src/VueRenderer.ts
@@ -1,5 +1,5 @@
 import type { DefineComponent, VNode } from 'vue'
-import { getCurrentInstance, h, markRaw, onBeforeMount, onUnmounted, ref } from 'vue'
+import { getCurrentInstance, h, markRaw, onBeforeMount, onUnmounted, shallowRef } from 'vue'
 
 /**
  * @internal
@@ -19,6 +19,37 @@ export interface VueRenderer<Context> {
   updateContext: () => void
 }
 
+type PortalState = [keys: string[], components: VueRendererComponent[]]
+
+function updateRenderer(state: PortalState, renderer: VueRenderer<unknown>): PortalState {
+  const [keys, components] = state
+  const newKey = renderer.key
+  const newComponent = renderer.render()
+
+  const index = keys.indexOf(newKey)
+  if (index === -1) {
+    return [
+      [...keys, newKey],
+      [...components, newComponent],
+    ]
+  } else {
+    const newComponents = [...components]
+    newComponents[index] = newComponent
+    return [keys, newComponents]
+  }
+}
+
+function removeRenderer(state: PortalState, renderer: VueRenderer<unknown>): PortalState {
+  const [keys, components] = state
+  const index = keys.indexOf(renderer.key)
+  if (index === -1) return state
+  const newKeys = [...keys]
+  const newComponents = [...components]
+  newKeys.splice(index, 1)
+  newComponents.splice(index, 1)
+  return [newKeys, newComponents]
+}
+
 /**
  * @internal
  */
@@ -32,7 +63,7 @@ export interface VueRendererResult {
  * @internal
  */
 export function useVueRenderer(): VueRendererResult {
-  const portals = ref<Record<string, VueRendererComponent>>({})
+  const portals = shallowRef<PortalState>([[], []])
   const instance = getCurrentInstance()
   const update = markRaw<{ updater?: () => void }>({})
 
@@ -47,7 +78,7 @@ export function useVueRenderer(): VueRendererResult {
   })
 
   const renderVueRenderer = (renderer: VueRenderer<unknown>) => {
-    portals.value[renderer.key] = renderer.render()
+    portals.value = updateRenderer(portals.value, renderer)
 
     // Force update the vue component to render
     // Cursor won't move to new node without this
@@ -55,14 +86,17 @@ export function useVueRenderer(): VueRendererResult {
   }
 
   const removeVueRenderer = (renderer: VueRenderer<unknown>) => {
-    delete portals.value[renderer.key]
+    portals.value = removeRenderer(portals.value, renderer)
   }
 
   const render = () => {
     const children: VNode[] = []
-    for (const key in portals.value) {
-      const Portal = portals.value[key]
-      children.push(h(Portal, { key }))
+    const [keys, components] = portals.value
+    const length = keys.length
+    for (let i = 0; i < length; i++) {
+      const key = keys[i]
+      const Component = components[i]
+      children.push(h(Component, { key }))
     }
     return children
   }
